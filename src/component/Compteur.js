ab982e3a48e39bd5fb0465fa20e10744fbdbdebf
QuentinChevaux <quentin.chevaux@gmail.com>
2021-11-30 14:40:39 +0100

    React Hook

--- src/component/Compteur.js
+++ src/component/Compteur.js
@@ -1,8 +1,15 @@
-import React, { useState } from 'react'
+import userEvent from '@testing-library/user-event';
+import React, { useState, useEffect } from 'react'
 
 function Compteur() {
 
     const [count, setCount] = useState(0)
+
+    useEffect ( () => {
+
+        document.title = 'Vous avez cliqué ' + count + ' fois'
+
+    }, [count]) // Un useEffect à toujours besoin de dépendances, si [] alors l'effet ne s'execute qu'au départ de la page, sinon il faut mettre la variable ciblée
 
     return (
 
@@ -10,9 +17,9 @@ function Compteur() {
 
             <h2>Vous avez cliqué sur le bouton {count} fois</h2>
 
-            <button onClick = {() => setCount(count + 1 )}>Cliquez içi</button>
+            <button onClick = { () => setCount(count + 1 )}>Cliquez içi </button>
 
-            <button onClick = { () => setCount(0)}>Remettre le compteur à 0</button>
+            <button onClick = { () => setCount(0)}>Remettre le compteur à 0 </button>
 
         </div>
 
